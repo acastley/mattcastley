bac16d4bf3aa508e6af3b8669a48720c81962cbb
acastley <mattcastley@gmail.com>
2023-11-26 14:33:56 +1100

    updating lighting reel

--- app/(pages)/lighting/page.jsx
+++ app/(pages)/lighting/page.jsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/navbar";
 export default function Lighting () {
     return(
     <>
-        <iframe width="100%" height="90%" src="https://www.youtube.com/embed/yL4dTtVR4Vk?autoplay=1&si=Fh-NI7EcB6BywnBp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <h1>more content coming soon</h1>
+        <iframe width="100%" height="90%" src="https://www.youtube.com/embed/S-FqFJlOPYM?&autoplay=1si=0o5TWh98CV5W-YCq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </>
 )}
