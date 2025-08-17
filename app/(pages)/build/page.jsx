265c1f81d9b81e5472f0271945c27715e9cac055
acastley <119280532+acastley@users.noreply.github.com>
2025-08-17 20:48:54 +1000

    Delete app/(pages) directory

--- app/(pages)/build/page.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Build () {
-    return(
-    <>
-        <iframe width="100%" height="90%" src="https://www.youtube.com/embed/z9fG8RNdEPE?autoplay=1&si=uV8J6AnRXrJsbZDn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        more content coming soon
-    </>
-)}
--- app/(pages)/lighting/page.jsx
+++
@@ -1,8 +0,0 @@
-import Navbar from "@/components/navbar";
-
-export default function Lighting () {
-    return(
-    <>
-        <iframe width="100%" height="90%" src="https://www.youtube.com/embed/S-FqFJlOPYM?&autoplay=1si=0o5TWh98CV5W-YCq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </>
-)}
