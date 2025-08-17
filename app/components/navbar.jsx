87349bff20fba2cec60947d52183ceb6b1fa51c2
acastley <119280532+acastley@users.noreply.github.com>
2025-08-17 21:08:18 +1000

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -9,13 +9,9 @@ export default function Navbar() {
         <div className="outline-white">
             <nav className='sticky top-0 w-full z-10 bg-gradient-to-r from-pink-300 to-pink-100'>
             <div className='p-2 flex justify-between items-center w-full'>
-                <Link href="/home" className='font-extrabold text-pink-50 outline-pink-200 text-5xl lg:tracking-extra-widest sm:tracking-normal md:tracking-normal'>MATT CASTLEY</Link>
+                <Link href="/" className='font-extrabold text-pink-50 outline-pink-200 text-5xl lg:tracking-extra-widest sm:tracking-normal md:tracking-normal'>MATT CASTLEY</Link>
             <div className='flex-wrap items-center text-pink-300 outline-pink-100 space-x-4 space-y-2'>
-            <DropDownPort/>
-                {/* <Link href="/lighting" className='py-1 px-3 min-w-24 rounded-full outline-double'>LIGHTING</Link>
-                <Link href="/build" className='py-1 px-3 min-w-24 rounded-full outline-double'>BUILD</Link>
-                <Link href="/comp" className='py-1 px-3 min-w-24 rounded-full outline-double'>GEN</Link> */}
-                </div>
+            </div>
             </div>
             </nav>
         </div>
